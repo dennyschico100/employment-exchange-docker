ccff8186a2051b0136c16e975078532128599258
Dennys Cordova <dennyschico2@gmail.com>
2021-11-07 22:56:49 -0600

    style:added eslint

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
-require("dotenv").config();
-const express = require("express")
+require('dotenv').config();
+const express = require('express');
 
-const app = new express()
-app.set("PORT",process.env.port || 3000)
+const app = new express();
+app.set('PORT', process.env.port || 3000);
 
-app.get("/",(req,res)=>{
- res.send({message:"welcome"})
-})
-
+app.get('/', (req, res) => {
+  res.send({ message: 'welcome' });
+});
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const app = require("./app")
 
 const main = ()=>{
+    
     app.listen(app.get("PORT"),()=>{
         console.log(`server running on port ${app.get("PORT")}`)
-
     })
 }
 main()
